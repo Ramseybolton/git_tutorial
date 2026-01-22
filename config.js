289c42df218d9cc5d2934fb6d8b34a8e7af5a4d9
aayush fahh <aayushpokhrel785@gmail.com>
2026-01-23 00:39:29 +0900

    syncing with old repo

--- config.js
+++ config.js
@@ -2,3 +2,4 @@ console.log('config.js');
 console.log('the new github push');
 console.log('new version to overwrite the previous one');
 console.log('new push again')
+console.log('this one as syncing with old repoooo')
--- src/index.js
+++ src/index.js
@@ -4,3 +4,4 @@ console.log('i hope this works');
 console.log('brand new github push');
 console.log('new version to overwrite the previous one');
 console.log('new push again')
+console.log('this one as syncing with old repoooo')
